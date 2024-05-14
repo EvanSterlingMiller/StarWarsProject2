052ec259d6c8aab070ab166d304e0b1d775c31b5
Evan Miller <evan.s.miller3@gmail.com>
2024-05-14 14:13:43 -0400

    server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import {MongoClient, ObjectId} from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+// add collections 
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log('Star Wars is running')
+})
