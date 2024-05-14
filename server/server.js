df88ebe19f2247617eb07440457a51b5f81e84b1
Anders Boyum <Acboyum@gmail.com>
2024-05-14 15:19:12 -0400

    added planets/:id route and planets/:id/films route

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ dotenv.config();
 
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
+const planetsCollection = process.env.MONGO_PLANETS_COLLECTION;
+const planetsAndFilmsCollection = process.env.MONGO_PLANETS_FILMS;
 // add collections 
 
 const app = express();
@@ -17,14 +19,49 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-
 app.get('/api/planets', async (req, res) => {
-    const client = await MongoClient.connect(url);
-    const collection = client.db(dbName).collection("planets");
-    const planets = await collection.find().toArray();
+    console.log("Planets env", planetsCollection);
+    try {
+        const client = await MongoClient.connect(url);
+        const collection = client.db(dbName).collection(planetsCollection);
+        const planets = await collection.find().toArray();
 
-    res.status(201).send(planets);
-})
+        res.status(201).send(planets);
+    } catch (error) {
+        console.error("Coulnd't get planets data from database");
+        res.status(500).send("Internal server error while trying to fetch planets");
+    }
+});
+
+app.get("/api/planets/:planetId", async (req, res) => {
+    const {planetId} = req.params;
+    try {
+        const client = await MongoClient.connect(url);
+        const collection = client.db(dbName).collection(planetsCollection);
+        // const planets = await collection.find({_id: new Object(planetId)}).toArray();
+        const planets = await collection.find({id: Number(planetId)}).toArray();
+
+        res.status(201).send(planets);
+    } catch (error) {
+        console.error("Error connecting to database, couldn't get planets data");
+        res.status(500).send("Internal server error while trying to fetch planets");
+    }
+});
+
+app.get("/api/planets/:planetId/films", async (req, res) => {
+    const {planetId} = req.params;
+    try {
+        const client = await MongoClient.connect(url);
+        const collection = client.db(dbName).collection(planetsAndFilmsCollection);
+        const filmsAndPlanets = await collection.find({planet_id: Number(planetId)}).toArray();
+
+        // Need to query the planets and films collections to get their respective information
+        res.status(201).send(filmsAndPlanets);
+    } catch (error) {
+        console.error("Error connecting to database, couldn't get planets data");
+        res.status(500).send("Internal server error while trying to fetch planets");
+    }
+});
 
 app.listen(PORT, () => {
     console.log('Star Wars is running')
