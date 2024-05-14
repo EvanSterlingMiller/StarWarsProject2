9a37bc8b61f7847dfcdcde71f5e6489f82fe3830
Anders Boyum <Acboyum@gmail.com>
2024-05-14 14:35:30 -0400

    adjusted planets route to show planets data from swapi db

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import {MongoClient, ObjectId} from 'mongodb';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+// routes
+// import { films } from './routes/films';
+
 dotenv.config();
 
 const url = process.env.MONGO_DB_URL;
@@ -14,8 +17,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+
 app.get('/api/planets', async (req, res) => {
-    res.send("Hello, no planets are here please");
+    const client = await MongoClient.connect(url);
+    const collection = client.db(dbName).collection("planets");
+    const planets = await collection.find().toArray();
+
+    res.status(201).send(planets);
 })
 
 app.listen(PORT, () => {
