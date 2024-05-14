2a195fd9d6bb5c30a30228c8a7cd4222db40dfa6
Evan Miller <evan.s.miller3@gmail.com>
2024-05-14 15:04:29 -0400

    films

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
+const filmCollection =process.env.MONGO_FILM_COLLECTION
 // add collections 
 
 const app = express();
@@ -16,6 +17,32 @@ app.use(cors());
 
 app.get('/api/planets', async (req, res) => {
     res.send("Hello, no planets are here please");
+})
+
+app.get("api/films", async (req, res) => {
+    try {
+        const client = await new MongoClient(url);
+        const db = client.db(dbName)
+        const collection = db.collection(filmCollection);
+        const showFilms = await collection.find({}).toArray()
+        res.json(showFilms)
+    } catch (err) {
+        console.error("Error:", err)
+        res.status(500).send("Oops!")
+    }
+})
+app.get("api/films:id", async (req, res) => {
+    try {
+        const {filmId} = req.params.id
+        const client = new MongoClient(url);
+        const db = client.db(dbName)
+        const collection = db.collection(filmCollection);
+        const showFilms = await collection.find({_id: new ObjectId}).toArray()
+        res.json(showFilms)
+    } catch (err) {
+        console.error("Error:", err)
+        res.status(500).send("Oops!")
+    }
 })
 
 app.listen(PORT, () => {
