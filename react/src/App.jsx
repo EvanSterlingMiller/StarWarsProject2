bf45d0510d1a586cd2cb231b386a81720ac36a11
Evan Miller <evan.s.miller3@gmail.com>
2024-05-14 16:52:24 -0400

    more framework

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -8,26 +8,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+      <h1>Star Wars look up</h1>
+        <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/characters" element={<Characters />} />
+            <Route exact path="films" element={<Films />} />
+            <Route exact path="planets" element={<Planets />} />
+        </Routes>
+    
+      </Router>
     </>
   )
 }
