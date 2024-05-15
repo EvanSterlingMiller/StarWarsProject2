e4ba736138d209c61e53d82787ac9b2afbe9882e
Anders Boyum <Acboyum@gmail.com>
2024-05-15 11:07:48 -0400

    got the data to App.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,25 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react';
 import './App.css'
-import {BrowserRouter as Router, Routes, Route}from 'react-routter-dom'
+import {BrowserRouter as Router, Routes, Route}from 'react-router-dom'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [characters, setCharacters] = useState([]);
+  const [films, setFilms] = useState([]);
+  const [planets, setPlanets] = useState([]);
+
+
+  // function to make fetch requests and validate data from server
+  const getData = async () => {
+    const url = "http://localhost:3000/api";
+    try {
+      // Fetch and validate character data
+      const charRes = await fetch(`${url}/characters`);
+      if(!charRes.ok){
+        throw new Error("Error fetching character data");
+      }
+      const characters = await charRes.json();
+
+      // Fetch and validate film data
+      const filmRes = await fetch(`${url}/films`);
+      if(!filmRes.ok){
+        throw new Error("Error fetching film data");
+      }
+      const films = await filmRes.json();
+
+      // Fetch and validate planet data
+      const planetRes = await fetch(`${url}/planets`);
+      if(!planetRes.ok){
+        throw new Error("Error fetching planet data");
+      }
+      const planets = await planetRes.json();
+
+      setCharacters(characters);
+      setFilms(films);
+      setPlanets(planets);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  },[])
 
   return (
     <>
       <Router>
       <h1>Star Wars look up</h1>
         <Routes>
-            <Route exact path="/" element={<Home />} />
+            {/* <Route exact path="/" element={<Home />} />
             <Route exact path="/characters" element={<Characters />} />
             <Route exact path="films" element={<Films />} />
-            <Route exact path="planets" element={<Planets />} />
+            <Route exact path="planets" element={<Planets />} /> */}
         </Routes>
     
       </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
